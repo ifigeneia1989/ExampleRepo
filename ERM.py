78774a2709e7f4ebcd097f3e0ddc4c7ad7bae8e6
ifi.derekli@databricks.com <ifi.derekli@databricks.com>
2022-01-05 16:55:01 +0000

    ERM.py commit

---
+++ ERM.py
@@ -0,0 +1,41 @@
+# Databricks notebook source
+print("hello")
+
+# COMMAND ----------
+
+# MAGIC %r
+# MAGIC print("Hello World!")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select 1
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC show tables
+
+# COMMAND ----------
+
+import logging
+import sys
+
+#Creating and Configuring Logger
+
+Log_Format = "%(levelname)s %(asctime)s - %(message)s"
+
+logging.basicConfig(stream = sys.stdout, 
+                    filemode = "w",
+                    format = Log_Format, 
+                    level = logging.ERROR)
+
+logger = logging.getLogger()
+
+#Testing our Logger
+
+logger.error("Our First ERROR Message")
+
+# COMMAND ----------
+
+
